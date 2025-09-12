9ac504ebcdf9c4f938537000e42ff538f0ca9349
Marco <marcoguarato15@gmail.com>
2025-09-12 12:11:36 -0300

    Métodos string final
    Métodos explicados de uma forma melhor

--- fundamentos/8-metodos-string.py
+++ fundamentos/8-metodos-string.py
@@ -8,7 +8,8 @@ print(movieName.upper()) # retona a string toda maiúscula
 print(movieName.lower()) # retorna a string toda minúscula
 print(movieName.capitalize()) # retorna a string com a primeira letra maiúscula, e deixa o resto em minúsculas
 print(movieName.title()) # retorna a string com a primeira letra maiúscula
-print(movieName.find("u")) # retorna o indice do caractere fornecido
-print(movieDescription.count("t")) # se mais de um, retorna a quantidade de caracteres nessa string
+print(movieName.find("u")) # retorna o primeiro indice encontrado do caractere fornecido
+print(f'find: {movieDescription.find("u")}')
+print(movieDescription.count("u")) # se mais de um, retorna a quantidade de caracteres nessa string
 print(movieName.replace("op", "teste")) # troca os caracteres fornecidos pelos informados em seguida
 print(movieDescription.split(',')) # separa em um vetor as frases quebrando pelo valor fornecido removendo-o
