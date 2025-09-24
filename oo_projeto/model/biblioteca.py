adaf6ea62b7ef04eadf1eb85689815d2a9d645d8
Marco <marcoguarato15@gmail.com>
2025-09-24 15:54:25 -0300

    Update biblioteca.py

--- oo_projeto/model/biblioteca.py
+++ oo_projeto/model/biblioteca.py
@@ -4,21 +4,30 @@ class Biblioteca:
 
     def __init__(self, name):
         self.name = name
-        self.active = False
+        self.__active = False # '__' antes da variável significa que ela é privada não sendo mais possível alterá-la diretamente
+                              # Apenas um '_' significa que é protegida, não deve ser alterada diretamente mas ainda é possível
         Biblioteca.bibliotecas.append(self)
     
     def __str__(self):
         # return "Nome:",self.name ,"Ativo:",self.active # retorna uma tupla
-        return self.name + " | " +  str(self.active)
+        return self.name + " | " +  self.getActive
     
     def listLibraries():
+        print(f"{'Nome da biblioteca'.ljust(25)} | Status")
         for l in Biblioteca.bibliotecas:
-            print(f"{l.name} | {"Ativa" if l.active else "Inativa"}")
+            print(f"{l.name.ljust(25)} | {l.getActive}")
+
+    def setActive(self): # Método SET
+        self.__active = not self.__active
+
+    @property # Método GET
+    def getActive(self):
+        return "Ativa" if self.__active else "Inativa"
     
-cityLibrary = Biblioteca("Biblioteca da Cidade")
-shoppingLibrary = Biblioteca("Biblioteca do Shopping")
+    @property
+    def get__name__(self):
+        return __name__
+    
+print(__name__)
 
-print(cityLibrary)
-print(shoppingLibrary)
-
-Biblioteca.listLibraries()
+    
