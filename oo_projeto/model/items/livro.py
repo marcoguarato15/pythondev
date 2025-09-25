10928898b64a55a63ec9027e38752b8f61529238
Marco <marcoguarato15@gmail.com>
2025-09-25 09:46:15 -0300

    Update livro.py

--- oo_projeto/model/items/livro.py
+++ oo_projeto/model/items/livro.py
@@ -3,7 +3,9 @@ from model.items.item_biblioteca import ItemBiblioteca
 class Livro(ItemBiblioteca):
     def __init__(self, title, author, price, isbn):
         super().__init__(title, author, price)
-        self._isbn = isbn
+        self._isbn = isbn       # Padrao ISBN (entre parênteses é o número de caracteres)
+                                # Padrão no mínimo 13 caracteres (prefixo(3)(978/979), Grupo de registro(1-5), 
+                                # Elemento Registrante(-7), Número de publicação(-6), Dígito de controle(1))
 
     def __str__(self):
         return self._title + " " + self._author + " " + str(self._price) + " " + self._isbn
