23f64d95ac501cc2cf77e24f3dba495b01a32a7f
Marco <marcoguarato15@gmail.com>
2025-09-25 10:05:43 -0300

    Update add método abstrato
    Import ABC(abstract)

--- oo_projeto/model/items/item_biblioteca.py
+++ oo_projeto/model/items/item_biblioteca.py
@@ -1,5 +1,13 @@
-class ItemBiblioteca:
+from abc import ABC, abstractmethod
+
+# Necessário herdar de ABC para aplicar o metodo abstrato as subclasses
+class ItemBiblioteca(ABC):
     def __init__(self, title, author, price):
         self._title = title
         self._author = author
         self._price = price
+
+    # Método abstrato (implementação obrigatória de subclasses)
+    @abstractmethod
+    def applyDiscount(self):
+        pass
