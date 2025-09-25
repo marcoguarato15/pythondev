63a3c542b9e99b45f80712d01c1ade99507342cd
Marco <marcoguarato15@gmail.com>
2025-09-25 08:54:10 -0300

    Create revista.py

---
+++ oo_projeto/model/items/revista.py
@@ -0,0 +1,10 @@
+from model.items.item_biblioteca import ItemBiblioteca
+
+class Revista(ItemBiblioteca):
+    def __init__(self, title, author, price, edicao):
+        super().__init__(title, author, price)
+        self._edicao = edicao
+
+    def __str__(self):
+        return self._title + " " + self._author + " " + str(self._price) + " " + self._edicao
+    
