a0064eddea0da91795fa34dea2625819d421d2ad
Marco <marcoguarato15@gmail.com>
2025-09-29 11:05:14 -0300

    Update 11-insere_mongodb.py

--- bd/11-insere_mongodb.py
+++ bd/11-insere_mongodb.py
@@ -5,7 +5,7 @@ client = MongoClient()
 mydb = client.dbposts
 mycollection = mydb.posts
 
-mycollection.create_index([('author.email', ASCENDING)], unique=True)
+mycollection.create_index([('author.email', ASCENDING)], unique=True) # Criando um índice único para email neste documento
 mycollection.create_index([('author.name', ASCENDING)], unique=True)  # índice errado
 print(mycollection.index_information())                               # mostrar indices /para acesso o 
                                                                       # nome do índice fica como author.name_1 ("author.name", 1)
@@ -22,11 +22,11 @@ post1 = {
     }
 }
 
-# result = mycollection.insert_one(post1)
-# print(result.acknowledged) # Resultado em True/False se foi inserido com sucesso
-# if result.acknowledged:
-#     print("Documento inserido com sucesso")
-# else:
-#     print("Falha ao inserir documento")
+result = mycollection.insert_one(post1)
+print(result.acknowledged) # Resultado em True/False se foi inserido com sucesso
+if result.acknowledged:
+    print("Documento inserido com sucesso")
+else:
+    print("Falha ao inserir documento")
 
 client.close()
