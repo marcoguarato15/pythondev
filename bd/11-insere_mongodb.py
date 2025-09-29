9d1d2491868a6211b932ca70af2acaeb85ee0765
Marco <marcoguarato15@gmail.com>
2025-09-29 10:38:52 -0300

    Create 11-insere_mongodb.py Cria a coleção e o banco de dados

---
+++ bd/11-insere_mongodb.py
@@ -0,0 +1,21 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+mydb = client.dbposts
+mycollection = mydb.posts
+
+post1 = {
+    "title": "FastAPI",
+    "category": "Backend",
+    "author": {
+        "name": "Rodrigo",
+        "email": "rodrigo@email.com"
+    }
+}
+
+result = mycollection.insert_one(post1)
+# print(result.acknowledged) # Resultado em True/False se foi inserido com sucesso
+if result.acknowledged:
+    print("Documento inserido com sucesso")
+else:
+    print("Falha ao inserir documento")
