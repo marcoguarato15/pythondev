1b7efbbad08b3b09d4f859896fc59260e8ad914d
Marco <marcoguarato15@gmail.com>
2025-09-29 11:16:33 -0300

    Create 13-atualiza_mongodb.py

---
+++ bd/13-atualiza_mongodb.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+mydb = client.dbposts
+mycollections = mydb["posts"]
+
+old_value = {"level" : "Intermediario"}
+new_value = {"$set": {"level":"Iniciante"}}
+
+# acha o primeiro e o altera (filter, update)
+mycollections.update_one(old_value, new_value)
+
+result = mycollections.find()
+
+for p in result:
+    print(p)
