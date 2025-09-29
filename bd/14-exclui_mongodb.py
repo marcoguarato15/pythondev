4c91bbd6242e402e78953ab4636ed2ec8b660475
Marco <marcoguarato15@gmail.com>
2025-09-29 11:23:05 -0300

    Create 14-exclui_mongodb.py

---
+++ bd/14-exclui_mongodb.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+client = MongoClient()
+mydb = client.dbposts
+mycollections = mydb["posts"]
+
+myquery = {"category": "Backend"}
+
+x = mycollections.delete_one(myquery)
+print(f"Informações do delete: {x}")
+# Irá deletar apenas o primeiro objeto encontrado como é delete_one...
+# mesmo sem objetos ele retorna 'acknowledged=True' pois é bem sucedida mas sem valores excluídos...
+print(f"Número de exclusões: {x.deleted_count}")
