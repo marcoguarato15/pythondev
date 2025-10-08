f5df73e7da12aeb49cd4c6b865fa380c5995582f
Marco <marcoguarato15@gmail.com>
2025-10-08 10:19:26 -0300

    Update functions.py

--- teste_python/functions.py
+++ teste_python/functions.py
@@ -9,3 +9,15 @@ def list_length(list):
 
 def validate_email(email):
     return '@' in email and '.' in email
+
+def soma_lista(valores: list) -> bool:
+    if not all(isinstance(i, (int, float)) for i in valores):
+        raise ValueError("Todos os valores da lista devem ser numeros")
+    return sum(valores)
+
+def encontrar_valor(dicionario, chave):
+    if not isinstance(dicionario, dict):
+        raise ValueError("O primeiro argumento deve ser um dicionário")
+    # dict.get(chave, None) significa que irá pegar o valor da chave passada como parâmetro
+    return dicionario.get(chave, None)
+
