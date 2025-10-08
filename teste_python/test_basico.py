42ec97f0ee5f76a3de53e3acc43683a7985deec3
Marco <marcoguarato15@gmail.com>
2025-10-08 09:53:59 -0300

    Update test_basico.py

--- teste_python/test_basico.py
+++ teste_python/test_basico.py
@@ -2,3 +2,11 @@
 # caso falhe ele mostra o resultado esperado e onde está o erro
 def test_soma():
     assert sum([1, 4, 5]) == 10
+
+def is_positive(number):
+    return number > 0
+
+def test_is_positive():
+    assert is_positive(5) is True
+    assert is_positive(-2) is False
+    # assert is_positive(0) is True # teste dá como erro -> FAILED test_basico.py::test_is_positive - assert False is True
