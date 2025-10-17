edfb1a5291a980eebfa2610e809f0ac326b1ab61
Marco <marcoguarato15@gmail.com>
2025-10-17 14:10:38 -0300

    Criação do metodo post(criar) para cursos e implementação, download da biblioteca cryptography para autenticação correta com o banco (necessária)

--- APIFlask/api/schemas/curso_schema.py
+++ APIFlask/api/schemas/curso_schema.py
@@ -3,10 +3,10 @@ from ..models import curso_model
 from marshmallow import fields
 
 class CursoSchema(ma.SQLAlchemyAutoSchema):
-    model = curso_model.Curso
-    _load_instance = True
-
-    fields = ("id", "nome", "descricao","data_criacao")
+    class Meta:
+        model = curso_model.Curso
+        _load_instance = True
+        fields = ("id", "nome", "descricao","data_criacao")
 
     nome = fields.String(required=True)
     descricao = fields.String(required=True)
--- APIFlask/api/views/curso_views.py
+++ APIFlask/api/views/curso_views.py
@@ -1,8 +1,31 @@
 from flask_restful import Resource
 from api import api
+from ..schemas import curso_schema
+from ..entidades import curso
+from ..services import curso_service
+from flask import request, make_response, jsonify
 
 class CursoList(Resource):
     def get(self):
         return "Olá mundo!"
-    
+    def post(self):
+        # Cria o schema de validação de dados
+        cursoSchema = curso_schema.CursoSchema()
+        # Passa a validação
+        validate = cursoSchema.validate(request.json)
+        # Se tiver erro fale onde estão os erros e retorne junto o status 400
+        if validate:
+            return make_response(jsonify(validate),400)
+        else:
+            nome = request.json["nome"]
+            descricao = request.json["descricao"]
+            data_criacao = request.json["data_criacao"]
+
+            # Chama a entidade de Curso
+            novo_curso = curso.Curso(nome=nome, descricao=descricao, data_criacao=data_criacao)
+
+            resultado = curso_service.cadastrar_curso(curso=novo_curso)
+            res_criacao = cursoSchema.jsonify(resultado)
+            return make_response(res_criacao, 201)
+
 api.add_resource(CursoList, '/cursos')
