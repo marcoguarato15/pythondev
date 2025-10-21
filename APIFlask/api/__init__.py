c9007180e059efca7f8fc41250e41b0bacc2ea03
Marco <marcoguarato15@gmail.com>
2025-10-21 14:06:30 -0300

    Inicio da autenticação da api

--- APIFlask/api/__init__.py
+++ APIFlask/api/__init__.py
@@ -3,15 +3,17 @@ from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager
 import os
 
 app = Flask(__name__,  template_folder=os.path.join(os.path.dirname(__file__), '..', 'templates'))
 app.config.from_object('config')
-app.secret_key = '123'
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
 
 api = Api(app)
 
